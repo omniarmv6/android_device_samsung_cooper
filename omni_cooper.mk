61de5c54992fe1a5ebed3b522bcacfb993a41bc9
androidmeda <hephappy@gmail.com>
2014-02-18 23:05:06 +0200

    MardonHH/android_device_samsung_cooper-2
    Change-Id: Ic39ff28b0ebab9adceaea82cc20d12f72ec63e43

--- omni_cooper.mk
+++ omni_cooper.mk
@@ -15,10 +15,17 @@
 # Inherit device specific files
 $(call inherit-product, device/samsung/cooper/device_cooper.mk)
 
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
 # Overrides
-PRODUCT_NAME := full_cooper
+PRODUCT_NAME := omni_cooper
 PRODUCT_DEVICE := cooper
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-S5830
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_RELEASE_NAME := GalaxyAce
+PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5830
